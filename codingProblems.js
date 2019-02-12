956a7515ad36e374e901431585d2da2ec723034e
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-02-11 20:32:35 -0600

    added solution from hackerrank problem

--- codingProblems.js
+++ codingProblems.js
@@ -6,3 +6,32 @@ function simpleArraySum(ar) {
     })
     return sum;
 }
+
+
+
+// Complete the function compareTriplets in the editor below. It must return an array of two integers, the first being Alice's score and the second being Bob's.
+
+// compareTriplets has the following parameter(s):
+
+// a: an array of integers representing Alice's challenge rating
+// b: an array of integers representing Bob's challenge rating
+
+
+function compareTriplets(a, b) {
+    let resultsArray = [];
+    let aScore = 0;
+    let bScore = 0;
+    for (let i = 0; i < a.length; i++){
+        if (a[i] > b[i]) {
+            aScore++;
+        }
+        if (b[i] > a[i]) {
+            bScore++;
+        }
+    }
+    resultsArray[0] = aScore;
+    resultsArray[1] = bScore;
+    return resultsArray;
+}
+
+
