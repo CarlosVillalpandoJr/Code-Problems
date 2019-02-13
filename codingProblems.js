7dc2a670a73718c1259c2d022d76f1b2f6f03b0e
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-02-12 18:17:06 -0600

    added some of array solution

--- codingProblems.js
+++ codingProblems.js
@@ -35,3 +35,15 @@ function compareTriplets(a, b) {
 }
 
 
+// Calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+function aVeryBigSum(ar) {
+    let sum = 0;
+    ar.forEach(num => {
+    sum += num
+    })
+    return sum;
+
+}
+
+
